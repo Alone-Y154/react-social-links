5252011ac14ed8ecd682fae068d4aa9e2a4d9407
Alone-Y154 <yashwanthkris154@gmail.com>
2024-01-27 20:00:52 +0530

    path change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
   <div className="bg-[#141414] flex justify-center items-center w-screen h-screen">
     <div className="bg-[#1F1F1F] rounded-xl w-[327px] md:w-[384px] md:p-10 flex p-6 flex-col items-center justify-center gap-6">
-      <p className="w-[88px] h-[88px] bg-[url('./src/assets/me.jpg')] bg-cover rounded-full"></p>
+      <p className="w-[88px] h-[88px] bg-[url('/me.png')] bg-cover rounded-full"></p>
       <div className="flex flex-col gap-1 items-center justify-center">
           <p className="text-white text-2xl font-semibold">Yashwanth Krishna</p>
           <p className="text-[#C4F82A] text-sm font-bold ">Bangalore, India</p>
